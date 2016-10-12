1534186fd976c844c1611f460bcc5465287bb17a
Manoj Singh Negi <justanothermanoj@gmail.com>
2016-10-12 22:34:29 +0530

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Button extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return(
+			<button>
+				this is a button
+			</button>
+		);
+	}
+}
+
+export default Button;
