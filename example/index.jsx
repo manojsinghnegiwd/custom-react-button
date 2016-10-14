09ee858486e03e5ca846d2a8b451708a951ab7f8
Manoj Singh Negi <justanothermanoj@gmail.com>
2016-10-14 21:38:12 +0530

    added onClick function

--- example/index.jsx
+++ example/index.jsx
@@ -3,12 +3,28 @@ import ReacDOM from 'react-dom';
 import Button from './custom-react-button';
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			text: 'Button'
+		};
+		this._onClick = this._onClick.bind(this);
+	}
+
+	_onClick () {
+		let text = 'Clicked';
+		this.setState({
+			text
+		});
+	}
+
 	render() {
 
 		const props = {
 			color: '#9b59b6',
 			full: true,
 			round: true,
+			onClick: this._onClick,
 			style: {
 				fontSize: '30px'
 			}
@@ -16,7 +32,7 @@ class App extends React.Component {
 
 		return (
 			<Button {...props}>
-				Button
+				{this.state.text}
 			</Button>
 		)
 	}
